fc014b354b68af4627e3fedf22736ed87495aace
academyjost <academyjost@gmail.com>
2026-04-22 20:41:27 -0500

    git add . git commit -m "Agregando cambios a repaso.html y repaso.js" git push origin main

---
+++ repaso.js
@@ -0,0 +1,30 @@
+function mostrar(opcion) {
+    switch (opcion) {
+        case 1:
+            ejercicio1();
+            break;
+        case 2:
+            ejercicio2();
+            break;
+        case 3:
+            ejercicio3();
+            break;
+            default:
+                break;
+    }
+}
+function ejercicio1() {
+    for (let i=1; i<=5; i++) {
+        console.log(i);
+    }
+}
+function ejercicio2() {
+    for (let i = 100; i <= 1000; i+=100) {
+        console.log(i);
+    }   
+}
+function ejercicio3() {
+    for (let i = 1000; i >= 800; i-=100) {
+        console.log(i*2);
+    }
+}
