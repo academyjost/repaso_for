a97c366bc228e7c881ceacf91c81c3d7d700a18c
academyjost <academyjost@gmail.com>
2026-04-22 21:43:23 -0500

    git add . git commit -m "Agregando estilos y funcionalidades al proyecto de repaso" git push origin main

--- repaso.js
+++ repaso.js
@@ -25,11 +25,32 @@ function ejercicio2() {
 }
 function ejercicio3() {
     for (let i = 1000; i >= 800; i-=100) {
-        console.log(i*2);
+        console.log(i);
     }
 }
 function mostrarMensaje1() { 
-    for (let i=5; i<=2; i--) {
+    for (let i=5; i<8; i++) {
         console.log("Hola Mundo");
     }   
+}
+function mostrarMensaje2() {
+    for (let i=10; i<=5; i++) {
+        console.log("Hola Mundo");
+    }
+}
+function mostrarMensaje3() {
+    for (let i=10; i>=5; i--) {
+        console.log("Hola Mundo");
+    }
+}
+function mostrarMensaje4() {
+    for (let i=5; i>10; i++) {
+        console.log("Hola Mundo");
+    }
+}
+//tabla de multiplicar del 3
+function mostrarTabla() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i + " x 3 = " + (i * 3));
+    }
 }
